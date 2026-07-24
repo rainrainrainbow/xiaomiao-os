f6ecb147b7c224a2c67465167fbb1e4d1a25992a
rainrainrainbow <1448282415@qq.com>
2026-07-24 11:28:22 +0800

    Add missing esp_timer.h include

--- main/power_manager/power_manager.c
+++ main/power_manager/power_manager.c
@@ -8,6 +8,7 @@
 #include "esp_adc/adc_oneshot.h"
 #include "esp_adc/adc_cali.h"
 #include "esp_adc/adc_cali_scheme.h"
+#include "esp_timer.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
