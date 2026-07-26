fdf2aa20f75497a427529c094b6c89c1ae33c2e0
rainrainrainbow <1448282415@qq.com>
2026-07-27 01:50:03 +0800

    Remove old file: main/desktop/statusbar.h

--- main/desktop/statusbar.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "lvgl.h"
-
-void statusbar_create(void);
-void statusbar_update(void);
-void statusbar_set_time(const char *time_str);
-void statusbar_set_battery(int percent);
