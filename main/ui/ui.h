c158beeb6803135b521f0290af59380243ba1fc9
rainrainrainbow <1448282415@qq.com>
2026-07-27 01:24:03 +0800

    Add main/ui/ui.h

---
+++ main/ui/ui.h
@@ -0,0 +1,10 @@
+#pragma once
+/*
+ * ui.h - UI 框架统一入口
+ */
+#include "lvgl.h"
+#include "ui/theme.h"
+#include "ui/fonts.h"
+#include "ui/components.h"
+#include "ui/page_manager.h"
+#include "ui/widgets.h"
